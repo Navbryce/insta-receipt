9864a03f3eff737086437b88f9b36119781be027
Navbryce <plunkett.bryce.m@gmail.com>
2022-02-01 21:33:13 -0500

    Improve exports

--- insta_receipt/__init__.py
+++ insta_receipt/__init__.py
@@ -1,3 +1,5 @@
-__version__ = "0.1.0"
+from .google_spreadsheet_generator import GoogleSpreadSheetGenerator
+
+__version__ = "0.1.2"
 
 TEMPLATE_PATH = "sheet_templates.json"  # TEMPLATE_PATH points to the fixed templates
